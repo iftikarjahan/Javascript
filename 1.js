13d3904428f95ebdf2001d9cd3c8135aaa690ce9
iftikarjahan <iftikarjahan22@gmail.com>
2022-02-08 22:29:01 +0530

    basics of declaring variables

--- 1.js
+++ 1.js
@@ -1,1 +1,18 @@
-console.log("Iftikar");
+// In JS we can declare variable in three ways
+// var let const
+
+//varuiable declaration
+var a; //here we dont need to specify the data type
+
+//variable initialisation
+a=10;
+
+//print statement
+//console.log(a);
+
+
+a="hello";
+console.log(a);
+
+// type off operator->tells us the type of data stored in a variable
+console.log(typeof a);
