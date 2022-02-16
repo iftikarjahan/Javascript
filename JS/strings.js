700f47c12f14a23425dd10332031a00a6f7e5a39
iftikarjahan <iftikarjahan22@gmail.com>
2022-02-16 12:03:53 +0530

    strings.js

---
+++ JS/strings.js
@@ -0,0 +1,15 @@
+var message="Hello World, hope everyone is coding";
+//string extraction methods
+
+//dont hamper the data you get....so you need to store the sliced part in a new variable
+
+//slice methods->you can use it in 3 ways
+var slicedString=message.slice(8);
+console.log(slicedString);
+
+//to lower and upper case
+var str2="fomo";
+console.log(str2.toUpperCase());
+
+var str3="YOLO";
+console.log(str3.toLowerCase());
