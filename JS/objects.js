e8bf9891f941156cbe4886f97caf436646a11af6
iftikarjahan <iftikarjahan22@gmail.com>
2022-02-21 13:44:10 +0530

    Day8: Objects In Javascript

--- JS/objects.js
+++ JS/objects.js
@@ -1,10 +1,12 @@
 // Javascript object are always in key value pairs
+// Everything in Javascript is an object
 // In javascript, we can access the properties using dot notation and square baracket notation
 let obj={};
 // console.log(obj);
 
 let person={
     //key: value
+    // if the keys are numbers, then its better to use bracket notation
     name: "Iftikar",
     age: 21,
     height: 168,
@@ -40,3 +42,13 @@ console.log(captainAmerica.address.state);
 
 //functions inside the objects are called methods
 captainAmerica.displayName();
+
+//looping through an object
+for(let ppp in captainAmerica){
+    // the in keyword refers to the individual key in the object
+    console.log(ppp);
+    //if i need the value of the key, we would use the bracket notation
+    console.log(captainAmerica[ppp]);
+}
+delete captainAmerica.lastName;
+console.log(captainAmerica);
